066349663c15d789f35877a9927e68a77eb7a8ae
naveen230 <31323987+naveen230@users.noreply.github.com>
2017-09-07 15:46:48 -0400

    Update cowrie.sh

--- cowrie.sh
+++ cowrie.sh
@@ -16,3 +16,22 @@ mv -- /tmp/output_file ./start.sh
 rm -rf /tmp/output_file
 #restart ssh
 service ssh restart
+#install Cowrie-log-viewer
+cd ..
+git clone https://github.com/mindphluxnet/cowrie-logviewer
+cd cowrie-logviewer
+pip install -r requirements.txt
+#install IPGeolocator
+mkdir maxmind
+cd maxmind
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
+gunzip GeoLite2-Country.mmdb.gz
+cd ..
+cd ..
+cd ..
+cd log
+touch cowrie.json
+touch cowrie.log
+cd ..
+cd cowrie-logviewer
+python cowrie-logviewer.py
