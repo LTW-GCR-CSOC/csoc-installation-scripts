6e8f4e76dc0bb16b60efd692e2c2a54ae0a1d75a
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-12 15:53:54 -0400

    Update honeypots.sh

--- honeypots.sh
+++ honeypots.sh
@@ -19,9 +19,9 @@ sudo apt-get update -y && apt-get upgrade
 #install dependencies
 sudo apt-get install -y git python-dev python-openssl openssh-server python-pyasn1 python-twisted authbind
 #set cowrie to listen to port22
-touch /etc/authbind/byport/22
-chown cowrie /etc/authbind/byport/22
-chmod 777 /etc/authbind/byport/22
+sudo touch /etc/authbind/byport/22
+sudo chown cowrie /etc/authbind/byport/22
+sudo chmod 777 /etc/authbind/byport/22
 #install cowrie
 git clone https://github.com/cowrie/cowrie.git
 cd cowrie
@@ -40,7 +40,7 @@ pip install -r requirements.txt
 #install IPGeolocator
 mkdir maxmind
 cd maxmind
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
+wget -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
 gunzip GeoLite2-Country.mmdb.gz
 cd ..
 cd ..
