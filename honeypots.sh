2f3268aab9274e15d486a596b88310a9ac994e2f
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-15 13:54:08 -0400

    Update honeypots.sh

--- honeypots.sh
+++ honeypots.sh
@@ -159,7 +159,7 @@ EOF
 echo "-----@ DIONAEA RUNNING CHECK -----" >>~/SETUP-RUN.TXT
 sudo ps -ef | grep dionaea >>~/SETUP-RUN.TXT
 # ensure seeing dionaea logs
-ls -l /opt/dionaea/var/dionaea.log >>~/SETUP-RUN.TXT
+ls -l /opt/dionaea/var/dionaea/dionaea.log >>~/SETUP-RUN.TXT
 
 # ---------------
 #
