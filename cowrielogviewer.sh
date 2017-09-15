9bbd233c92e0ad31d3e0679a7163d936d59d714c
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-15 15:08:39 -0400

    Update cowrielogviewer.sh

--- cowrielogviewer.sh
+++ cowrielogviewer.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-exit
+#install Cowrie-log-viewer
+cd ..
+git clone https://github.com/mindphluxnet/cowrie-logviewer
+cd cowrie-logviewer
+pip install -r requirements.txt
+#install IPGeolocator
+mkdir maxmind
+cd maxmind
+wget -N http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
+unzip -o GeoLite2-Country.mmdb.gz
+cd ..
+cd ..
+cd cowrie/log
+touch cowrie.json
+touch cowrie.log
+cd ..
+cd ..
+cd cowrie-logviewer
+python cowrie-logviewer.py 
