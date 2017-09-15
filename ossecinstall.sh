96227a4e954ba051b84f84cd8e33e834fd38a2e9
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-15 10:26:30 -0400

    Create ossecinstall.sh

---
+++ ossecinstall.sh
@@ -0,0 +1,28 @@
+# ---------------
+# install ossec
+# https://ossec.github.io/index.html
+#
+echo "-----@ INSTALL OSSEC -----" >>~/SETUP-RUN.TXT
+cd ~
+sudo apt-get -y install build-essential
+sudo apt-get -y install mysql-server
+wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
+tar -zxf ossec-hids-2.8.3.tar.gz
+cd ossec-hids-2.8.3
+./install.sh
+
+echo "-----@ OSSEC SETUP DONE -----" >>~/SETUP-RUN.TXT
+#
+# Configure OSSEC
+#
+# TODO: configure OSSEC
+#
+#--------
+#
+# start OSSEC
+#
+echo "-----@ START OSSEC -----" >>~/SETUP-RUN.TXT
+sudo /var/ossec/bin/ossec-control start
+#
+# TODO: clean-up remove all files (e.g. source downloads) that not required for production operation
+#
