8f766b3f4141d6286546e3869539052139a29947
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-18 14:44:08 -0400

    Update cowrielogviewer.sh

--- cowrielogviewer.sh
+++ cowrielogviewer.sh
@@ -6,8 +6,8 @@
 #----------------
 # TODO remove old directories to do a clean install
 # run as cowrie user su cowrie
+rm -rf /home/cowrie/cowrie-logviewer
 su - cowrie <<!
-cowrie
 cd /home/cowrie
 git clone https://github.com/LTW-GCR-CSOC/cowrie-logviewer.git
 cd cowrie-logviewer
@@ -15,10 +15,9 @@ chmod +x cowrie-logviewer.py
 pip install -r requirements.txt
 mkdir -p maxmind
 cd maxmind
+wget -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-County.mmdb.gz
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
 gunzip GeoLite2-Country.mmdb.gz
-rm GeoLite2-Country.mmdb.gz
-
 cd /home/cowrie/cowrie-logviewer
-nohup python cowrie-logviewer.py & 
+nohup python cowrie-logviewer.py &
 !
