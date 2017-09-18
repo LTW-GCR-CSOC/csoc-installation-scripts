4de406686bc9cceb4b159d7df788b848e6627dd1
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-18 11:56:13 -0400

    Update cowrieinstall.sh

--- cowrieinstall.sh
+++ cowrieinstall.sh
@@ -15,15 +15,17 @@ sudo apt-get install -y python-pyasn1
 sudo apt-get install -y python-twisted 
 sudo apt-get install -y authbind
 
+sudo /usr/bin/easy_install virtualenv
+
 #change default port to Port 8742(to be tested with the pi)
 #sed -i '/^Port/c\Port 8742' /etc/ssh/sshd_config
 
+cd /home/cowrie
 git clone https://github.com/LTW-GCR-CSOC/cowrie.git
-cd cowrie
 virtualenv cowrie-env
 source cowrie-env/bin/activate
 export PYTHONPATH=/home/cowrie/cowrie
-bin/cowrie start
+/home/cowrie/cowrie/bin/cowrie start
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
 apt-get install authbind
 touch /etc/authbind/byport/22
