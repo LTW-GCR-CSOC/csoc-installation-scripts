f2487c5fff3db534cd936808a05b4e126f6db344
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-18 13:09:09 -0400

    Update cowrieinstall.sh

--- cowrieinstall.sh
+++ cowrieinstall.sh
@@ -7,27 +7,33 @@
 #----------------
 
 #install dependencies
-sudo apt-get install -y git 
-sudo apt-get install -y python-dev 
+sudo apt-get install -y git
+sudo apt-get install -y python-dev
 sudo apt-get install -y python-openssl
 sudo apt-get install -y openssh-server
-sudo apt-get install -y python-pyasn1 
-sudo apt-get install -y python-twisted 
+sudo apt-get install -y python-pyasn1
+sudo apt-get install -y python-twisted
 sudo apt-get install -y authbind
 
-sudo /usr/bin/easy_install virtualenv
+su - cowrie <<!
+cowrie
+/usr/bin/easy_install virtualenv
 
 #change default port to Port 8742(to be tested with the pi)
 #sed -i '/^Port/c\Port 8742' /etc/ssh/sshd_config
 
 cd /home/cowrie
+rm -rf cowrie
 git clone https://github.com/LTW-GCR-CSOC/cowrie.git
+cd cowrie
 virtualenv cowrie-env
 source cowrie-env/bin/activate
 export PYTHONPATH=/home/cowrie/cowrie
-/home/cowrie/cowrie/bin/cowrie start
+pip install -r requirements.txt
+bin/cowrie start
 #sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
 touch /etc/authbind/byport/22
 chown cowrie:cowrie /etc/authbind/byport/22
 chmod 770 /etc/authbind/byport/22
-
+!
+echo 'CHANGE cowrie password to no password'
