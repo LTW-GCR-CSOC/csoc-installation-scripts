18594018ffc5a68bd97059bf347e05013d0b7f05
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-22 10:00:32 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/LTW-GCR-CSOC/canary-installation-scripts/archive/master.zip   
+unzip master.zip -d gcrinstall   
+cd canary-installation-scripts-master/
+chmod +x *.sh
+./honeypots.sh
