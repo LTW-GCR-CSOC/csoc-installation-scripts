d4d0bbc80c8327292f699005b0fed7dfd83fe77c
LTW Global Cybersecurity Resource Project <31705587+LTW-GCR@users.noreply.github.com>
2017-09-22 10:01:41 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/LTW-GCR-CSOC/canary-installation-scripts/archive/master.zip   
-unzip master.zip -d gcrinstall   
-cd canary-installation-scripts-master/
-chmod +x *.sh
-./honeypots.sh
