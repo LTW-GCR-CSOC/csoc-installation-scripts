36a01bfe3beec5ac50fd33b5e9142e076555ab7b
Carleton University Global Cybersecurity Resource Project <31705587+CUGCR@users.noreply.github.com>
2017-09-27 10:59:11 -0400

    Update openvasinstallPPA.sh

--- openvasinstallPPA.sh
+++ openvasinstallPPA.sh
@@ -1,17 +1,17 @@
 # Reference: https://hackertarget.com/openvas-9-install-ubuntu-1604/
-add-apt-repository ppa:mrazavi/openvas
-apt -y update
-apt -y install sqlite3
-apt -y install openvas9
-apt -y install texlive-latex-extra --no-install-recommends
-apt-get -y install texlive-fonts-recommended
-apt -y install libopenvas9-dev
-greenbone-nvt-sync
-greenbone-scapdata-sync
-greenbone-certdata-sync
-service openvas-scanner restart
-ps -ef | grep openvas
+sudo add-apt-repository ppa:mrazavi/openvas
+sudo apt -y update
+sudo apt -y install sqlite3
+sudo apt -y install openvas9
+sudo apt -y install texlive-latex-extra --no-install-recommends
+sudo apt-get -y install texlive-fonts-recommended
+sudo apt -y install libopenvas9-dev
+sudo greenbone-nvt-sync
+sudo greenbone-scapdata-sync
+sudo greenbone-certdata-sync
+sudo service openvas-scanner restart
+sudo ps -ef | grep openvas
 # apt install smbclient
-service openvas-manager restart
-openvasmd --rebuild --progress
+sudo service openvas-manager restart
+sudo openvasmd --rebuild --progress
 # admin / admin https://<local>:4000
