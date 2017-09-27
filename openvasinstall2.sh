47aad31843b9ca9a6ced6081234251ab3b550b5a
Carleton University Global Cybersecurity Resource Project <31705587+CUGCR@users.noreply.github.com>
2017-09-27 10:02:23 -0400

    Update openvasinstall2.sh

--- openvasinstall2.sh
+++ openvasinstall2.sh
@@ -1,11 +1,11 @@
 # Reference: https://hackertarget.com/openvas-9-install-ubuntu-1604/
 add-apt-repository ppa:mrazavi/openvas
-apt update
-apt install sqlite3
-apt install openvas9
-apt install texlive-latex-extra --no-install-recommends
-apt-get install texlive-fonts-recommended
-apt install libopenvas9-dev
+apt -y update
+apt -y install sqlite3
+apt -y install openvas9
+apt -y install texlive-latex-extra --no-install-recommends
+apt-get -y install texlive-fonts-recommended
+apt -y install libopenvas9-dev
 greenbone-nvt-sync
 greenbone-scapdata-sync
 greenbone-certdata-sync
