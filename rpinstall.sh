9551da680b4bf6d121b3c8dac8e9c506481c1d76
as22323 <a.k.shah@ieee.org>
2017-10-05 12:33:02 -0400

    added: change timezone to utc, disable wifi and bt,

--- rpinstall.sh
+++ rpinstall.sh
@@ -17,3 +17,29 @@ sudo apt-get install watchdog
 # Uncomment the line that says #max-load-1 = 24 by removing the hash symbol to reboot the device if the load goes over 24 over 1 minute. A load of 25 of one minute means that you would have needed 25 Raspberry Pis to complete that task in 1 minute. You may tweak this value to your liking.
 #
 echo "-----@ INSTALL WATCHDOG FOR CANARY -----" >>~/SETUP-RUN.TXT
+
+#Change device time to UTC time
+sudo timedatectl set-timezone UTC
+
+#Application to enable changing of mac address
+sudo apt-get install -y macchanger
+
+#Change MAC address - this is for rpi
+#ifconfig enxb827eb9e5b73 down
+#sudo macchanger -m 94:2e:17:9E:5B:73 enxb827eb9e5b73
+#ifconfig enxb827eb9e5b73 up
+
+#Disable bluetooth and wifi
+echo '#GCR Disable bluetooth and wifi' | sudo tee --append /boot/config.txt
+echo 'dtoverlay=pi3-disable-bt' | sudo tee --append /boot/config.txt
+echo 'dtoverlay=pi3-disable-wifi' | sudo tee --append /boot/config.txt
+sudo systemctl disable hciuart
+echo '#GCR Disable bluetooth and wifi' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo '#disable wifi' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo 'blacklist brcmfmac' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo 'blacklist brcmutil' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo '#disable bluetooth' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo 'blacklist btbcm' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+echo 'blacklist hci_uart' | sudo tee --append /etc/modprobe.d/raspi-blacklist.conf
+
+
