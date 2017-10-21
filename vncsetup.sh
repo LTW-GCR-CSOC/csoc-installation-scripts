4bf2a08012f8bd4afd3cdae369b1d4158200cd01
Carleton University Global Cybersecurity Resource Project <31705587+CUGCR@users.noreply.github.com>
2017-10-21 11:58:33 -0400

    Create vncsetup.sh

---
+++ vncsetup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Ref: https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-16-04
+# Install VNC server for use on Raspberry Pi for development testing
+sudo apt install xfce4 xfce4-goodies tightvncserver
+vncserver
+vncserver -kill :1
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+cd ~/.vnc
+wget https://raw.githubusercontent.com/LTW-GCR-CSOC/csoc-installation-scripts/master/InstallerFiles/xstartup
+sudo chmod +x ~/.vnc/xstartup
+vncserver
