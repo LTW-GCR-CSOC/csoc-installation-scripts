35309df2920952541961c97dea0b4ef425b7d51a
Brandon Hurley <brandonhurley2011@gmail.com>
2017-11-03 12:04:34 -0400

    Updated to version 0.4
    Moved install commands to https://github.com/LTW-GCR-CSOC/openvas-commander/blob/master/openvas_commander.sh. Also disabled the autocleanup commands as they need further testing.

--- openvasinstall.sh
+++ openvasinstall.sh
@@ -2,7 +2,7 @@
 #
 # OpenVAS install
 # Reference: https://avleonov.com/2017/04/10/installing-openvas-9-from-the-sources/
-# v0.3
+# v0.4
 
 # Variables
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
@@ -15,11 +15,10 @@ if [[ "$EUID" -ne 0 ]]; then
   printf "**** ${RED}Sorry, you need to run this as root${NC} ****\n"
   exit 2
 fi
-  
-# Install dependencies
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+  
+# Install dependencies
 echo "Starting the OpenVAS install. It will take between 1-3 hours to install"
 
 echo "Installing curl and CMake..."
@@ -27,46 +26,15 @@ sudo apt-get -y install curl
 sudo apt-get -y install cmake
 echo "Finished installing curl and CMake"
 
-# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-redis-on-ubuntu-16-04
-# Add Creative Commons note here
-# Install Redis from source
-echo "Installing Redis..."
-sudo apt-get -y update
-sudo apt-get -y install build-essential tcl
-cd /tmp
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xzvf redis-stable.tar.gz
-cd redis-stable
-
-make
-make test
-sudo make install
-
-sudo mkdir /etc/redis
-cd /etc/redis
-wget https://raw.githubusercontent.com/LTW-GCR-CSOC/csoc-installation-scripts/master/InstallerFiles/redis.conf
-cd /etc/systemd/system/
-wget https://raw.githubusercontent.com/LTW-GCR-CSOC/csoc-installation-scripts/master/InstallerFiles/redis.service
-
-sudo adduser --system --group --no-create-home redis
-sudo mkdir /var/lib/redis
-sudo chown redis:redis /var/lib/redis
-sudo chmod 770 /var/lib/redis
-
-sudo systemctl start redis
-
-sudo systemctl enable redis
-
-echo -n "Redis is currently: "
-sudo systemctl is-active redis
-echo "Finished installing Redis"
-
-
 cd $DIR
 wget https://raw.githubusercontent.com/LTW-GCR-CSOC/openvas-commander/master/openvas_commander.sh
 chmod +x openvas_commander.sh
 
 # Install dependencies (the longest operation):
+echo "Installing Redis..."
+sudo ./openvas_commander.sh --install-redis-source
+echo "Finished installing Redis"
+
 echo "Installing dependencies..."
 sudo ./openvas_commander.sh --install-dependencies
 echo "Finished installing dependencies"
@@ -96,7 +64,7 @@ sudo ./openvas_commander.sh --install-all
 
 # NB: If you are afraid that something might go wrong, you can start separately:
 
-# ./openvas_commander.sh --install-component "openvas-smb"
+#sudo ./openvas_commander.sh --install-component "openvas-smb"
 #sudo ./openvas_commander.sh --install-component "openvas-libraries"
 #sudo ./openvas_commander.sh --install-component "openvas-scanner"
 #sudo ./openvas_commander.sh --install-component "openvas-manager"
@@ -104,10 +72,11 @@ sudo ./openvas_commander.sh --install-all
 #sudo ./openvas_commander.sh --install-component "greenbone-security-assistant"
 echo "Finished installing the OpenVAS components"
 
-echo "Cleaning up leftover files..."
-sudo apt-get autoremove
-sudo apt-get clean
-echo "Finished cleaning up leftover files..."
+# Cleanup needs further testing
+#echo "Cleaning up leftover files..."
+#sudo apt-get autoremove
+#sudo apt-get clean
+#echo "Finished cleaning up leftover files..."
 
 # Create certificates and a user:
 
@@ -125,7 +94,7 @@ echo "Rebuilding content..."
 sudo ./openvas_commander.sh --rebuild-content
 echo "Finished rebuilding content"
 
-# Check, that everything is started:
+# Check that everything is started:
 
 ./openvas_commander.sh --check-proc
 
@@ -138,12 +107,9 @@ sleep 3m
 # If something goes wrong, you can always find out what to do next with:
 sudo ./openvas_commander.sh --check-status v9
 
-# Install the autostart script
+# Install the service
 echo "Setting up OpenVAS for autostart on boot..."
-cd /etc/init.d/
-sudo wget https://raw.githubusercontent.com/LTW-GCR-CSOC/csoc-installation-scripts/master/InstallerFiles/openvas_start
-sudo chmod +x openvas_start 
-sudo update-rc.d openvas_start defaults
+sudo ./openvas_commander.sh --install-service
 
 cd $DIR
 echo 'OpenVAS is now ready to use! Open a web browser and go to "localhost" to access the web interface.'
