8f29f5cb5f5b1399269cbe9c7bb0fee43b5bf4fd
as22323 <a.k.shah@ieee.org>
2017-12-15 01:14:56 +0000

    setup throttling rules

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,4 @@
-#setup firewall
+##setup firewall
 
 # setup iptables-persistent
 sudo apt-get -y install iptables-persistent
@@ -29,8 +29,37 @@ sudo iptables -A INPUT -p UDP -m multiport --dports 22,1434,443,5060,5061,1900,6
 sudo iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 sudo iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
+##Throttle data
+export NIC=<i.e. eth1>
+export ALLOW_TCP_PORTS=2202,23,22,25,8080,80,443,5060,5061,1900,69,139,445
+export ALLOW_UDP_PORTS=22,1434,443,5060,5061,1900,69,139,44
+export NICTHROTTLE1=683bps
+export NICTHROTTLE2=1000kbps
+export NICTHROTTLE3=1000kbps
+export MESSAGEPORT1=<port to sent to metron>
+export MESSAGEPORT2=<port to sent to metron>
+
+#clear settings
+sudo tc qdisc del dev $NIC root
+
+#define throttling
+sudo tc qdisc add dev $NIC root handle 1: htb
+sudo tc class add dev $NIC parent 1: classid 1:1 htb rate $NICTHROTTLE1
+sudo tc class add dev $NIC parent 1:1 classid 1:5 htb rate $NICTHROTTLE2 ceil $NICTHROTTLE2 prio 1
+sudo tc class add dev $NIC parent 1:1 classid 1:6 htb rate $NICTHROTTLE3 ceil $NICTHROTTLE3 prio 0
+sudo tc filter add dev $NIC parent 1:0 prio 1 protocol ip handle 5 fw flowid 1:5
+sudo tc filter add dev $NIC parent 1:0 prio 0 protocol ip handle 6 fw flowid 1:6
+sudo iptables -A OUTPUT -t mangle -p tcp --sport $MESSAGEPORT1 -j MARK --set-mark 5
+sudo iptables -A OUTPUT -t mangle -p tcp --sport $MESSAGEPORT2  -j MARK --set-mark 6
+
+#show results 
+#sudo tc qdisc show
+
 #Save rules and make them persistant
 sudo netfilter-persistent save
+service iptables save
+/usr/libexec/iptables/iptables.init save
+service iptables reload
 
 # show iptables setup
 sudo iptables -S
